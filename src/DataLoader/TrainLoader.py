3ed230fe70705591e9e5bbd124cd69fd10689582
ivanrj7j <ivanrj7j@gmail.com>
2023-10-28 19:45:24 +0530

    commit

--- src/DataLoader/TrainLoader.py
+++ src/DataLoader/TrainLoader.py
@@ -1,3 +1,4 @@
+import numpy as np
 from DataLoader import Paginator
 from Tokenizer import NLPTokenizer
 
@@ -23,10 +24,39 @@ class TrainLoader(Paginator):
         sequenceLength (int) -- Maximum tokens
 
         shouldShuffle -- shuffles data if true
-        
+
         Return: None
         """
         
         super().__init__(batchSize, shouldShuffle)
         self.tokenizer = NLPTokenizer(tokenizerFile, sequenceLength)
+        self.sequenceLength = sequenceLength
+
+    def nextBatch(self) -> np.ndarray:
+        return super().nextBatch()
+    
+    def trainTokenizeText(self, text:str) -> np.ndarray:
+        """
+        Tokenizes a text for passing through training 
+        
+        Keyword arguments:
+
+        text -- text to be tokenized
+
+        Return: a matrix of tokens
+        """
+        
+        encodedMatrix = self.tokenizer.encode(text, False)
+        x = []
+        y = []
+        for vector in encodedMatrix:
+            for i in range(1, len(vector)):
+                start = max(0, i-self.sequenceLength)
+                data = vector[start:i]
+                if len(data) < self.sequenceLength:
+                    data = np.pad(data, (self.sequenceLength-len(data), 0), 'constant', constant_values=-1)
+                x.append(data)
+                y.append(vector[i])
+        
+        return np.vstack(x).astype(np.int32), np.array(y, dtype=np.int32)
         
