4c3c1f061f9a582d8c9b6265e6cb1fe435344361
ivanrj7j <ivanrj7j@gmail.com>
2023-10-29 19:40:29 +0530

    started experminting with model builders

---
+++ src/ModelBuilds/__init__.py
@@ -0,0 +1,1 @@
+from ModelBuilds.funcs import createModel
---
+++ src/ModelBuilds/funcs.py
@@ -0,0 +1,14 @@
+from keras.layers import Input, Embedding, Dense
+from keras.models import Sequential, Model
+
+def createModel(sequenceLength:int, totalTokens:int, embeddingOutput:int, hiddenLayer:Sequential, name:str) -> Model:
+    inputLayer = Input(sequenceLength, name="input_layer")
+
+    embeddingLayer = Embedding(sequenceLength, embeddingOutput, input_length=totalTokens, name="embedding_layer")(inputLayer)
+    
+    hiddenLayer = hiddenLayer(embeddingLayer)
+
+    outputLayer = Dense(totalTokens, activation="softmax", name="output")(hiddenLayer)
+
+    return Model(inputs=inputLayer, outputs=outputLayer, name=name)
+
