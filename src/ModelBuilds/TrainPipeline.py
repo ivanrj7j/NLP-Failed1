339ed5aa5a6df3b579ebf8dcbea697ff6939d177
ivanrj7j <ivanrj7j@gmail.com>
2023-11-04 15:13:46 +0530

    added callbacks

--- src/ModelBuilds/TrainPipeline.py
+++ src/ModelBuilds/TrainPipeline.py
@@ -1,4 +1,5 @@
 from keras.models import Sequential, Model
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
 import json
 import os
 from string import ascii_lowercase
@@ -15,7 +16,7 @@ class TrainPipeline:
 
     This stores all the evaluation data.
     """
-    def __init__(self, model:Sequential, data, name:str=None, validData=None) -> None:
+    def __init__(self, model:Sequential, data, path:str, name:str=None, validData=None) -> None:
         """
         Initiates the pipeline
         
@@ -24,6 +25,8 @@ class TrainPipeline:
         model -- Sequential Model
 
         data -- Data to be used for training
+
+        path -- Path to save the model
 
         name -- Name of the model
 
@@ -37,6 +40,7 @@ class TrainPipeline:
         self.validData = validData
         self.model = self.buildModel()
         self.history = {}
+        self.path = path
 
     def createName(self):
         if self.name is None or self.name == "":
@@ -81,26 +85,27 @@ class TrainPipeline:
         self.model.compile(optimizer, loss, metrics)
         # compiling model 
 
+        lrReduce = ReduceLROnPlateau(patience=5)
+        checkPoint = ModelCheckpoint(os.path.join(self.path, f"{self.name}_checkpoint.keras"), save_best_only=True)
+        earlyStopping = EarlyStopping(patience=5)
+
+        callbacks = [lrReduce, checkPoint, earlyStopping]
+
         if self.validData is None:
-            history = self.model.fit(self.data, epochs=epochs, shuffle=False)
+            history = self.model.fit(self.data, epochs=epochs, shuffle=False, callbacks=callbacks)
         else:
-            history = self.model.fit(self.data, epochs=epochs, shuffle=False, validation_data=self.validData)
+            history = self.model.fit(self.data, epochs=epochs, shuffle=False, validation_data=self.validData, callbacks=callbacks)
         # training model 
 
         self.historyCallback(history)
         # saving history 
 
-    def save(self, path:str):
+    def save(self):
         """
         Saves the model and evaluation data as an h5 and json file
-        
-        Keyword arguments:
-
-        path -- path to saving the model
-
-        Return: None
         """
 
-        self.model.save(os.path.join(path, f"{self.name}.keras"))
-        with open(os.path.join(path, f"{self.name}_eval.json"), "w") as f:
+        self.model.save(os.path.join(self.path, f"{self.name}.keras"))
+
+        with open(os.path.join(self.path, f"{self.name}_eval.json"), "w") as f:
             json.dump(self.history, f)
